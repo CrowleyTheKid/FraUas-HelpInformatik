b2d8d757d2bc48e78a59291cfd8be33658c51400
SimonGloser <simon.gloser@googlemail.com>
2019-10-04 17:24:26 +0200

    styled footer

--- helpInformatik/resources/views/partials/footer.blade.php
+++ helpInformatik/resources/views/partials/footer.blade.php
@@ -1,2 +1,6 @@
-
-<p>footer</p>
+<footer>
+    <div class="container">
+        <div class="item"><a href="#">About</a></div>
+        <div class="item">Copyright &copy; 2015, All Rights Reserved</div>
+    </div>
+</footer>
