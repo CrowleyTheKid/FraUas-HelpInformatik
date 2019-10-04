90ecb7dfa7720aad2251249ab84c9edeeedde4ad
SimonGloser <simon.gloser@googlemail.com>
2019-10-04 19:35:11 +0200

    update named route home

--- helpInformatik/routes/web.php
+++ helpInformatik/routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'PagesController@home');
+Route::get('/', 'PagesController@home')->name('home');
 
 Route::get('/{page}', ['uses' => 'PagesController@page'])
     ->where('page', '[A-Za-z]+');
